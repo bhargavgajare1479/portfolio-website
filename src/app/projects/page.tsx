845686fa0ab9256e1bc323075b9b242d3839e044
Bhargav Gajare <59600950+bhargavgajare1479@users.noreply.github.com>
2025-04-28 01:24:31 +0530

    Fix card length on /project section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,7 +18,7 @@ export default function ProjectsPage() {
         <p className={`mt-4 text-base sm:text-lg md:text-xl text-center text-muted-foreground ${outfit.className}`}>Turning concepts into creations — and making them count.</p>
         <div className="mt-10 grid grid-cols-1 gap-8">
           {projectsData.map((project, idx) => (
-            <Card key={idx} className="flex flex-col gap-3">
+            <Card key={idx} className="w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl mx-auto p-4 text-start flex flex-col gap-3">
               <div className="flex items-center gap-2">
                 <h2 className={`text-2xl font-bold ${lexend.className}`}>{project.name}</h2>
                 {project.github && (
